bc87b46ae68f787b09885729717e565228d63a40
wesley <w-ess@hotmail.com>
2019-01-26 19:29:02 -0200

    ediçao e exclusao da lista

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from '../shared/category-module';
+import { CategoryService } from '../shared/category.service';
+import { element } from '@angular/core/src/render3';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('erro ao carregar lista')
+    );
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        // tslint:disable-next-line:no-shadowed-variable
+        () => this.categories = this.categories.filter(element => element !== category),
+        () => alert('erro ao tentar excluir!')
+      );
+    }
   }
 
 }
