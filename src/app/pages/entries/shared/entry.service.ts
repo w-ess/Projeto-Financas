c4eb6409423a2a48621b93330eef134baa4f181f
wesley <w-ess@hotmail.com>
2019-01-29 22:36:32 -0200

    adding EntryListComponent

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -18,7 +18,7 @@ export class EntryService {
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
       catchError(this.handleError),
-      map(this.jsonDataToCategories)
+      map(this.jsonDataToEntries)
     );
   }
 
@@ -56,15 +56,18 @@ export class EntryService {
   }
 
   // PRIVATE METHODS
-  private jsonDataToCategories(jsonData: any[]): Entry[] {
+  private jsonDataToEntries(jsonData: any[]): Entry[] {
      const entries: Entry[] = [];
-     // tslint:disable-next-line:no-shadowed-variable
-     jsonData.forEach(element => entries.push(element as Entry));
+
+     jsonData.forEach(element => {
+       const entry = Object.assign(new Entry(), element)
+       entries.push(entry);
+     });
      return entries;
   }
 
   private jsonDataToEntry(jsonData: any): Entry {
-    return jsonData as Entry;
+    return Object.assign(new Entry(), jsonData);
   }
 
   private handleError(error: any): Observable<any> {
